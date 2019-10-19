9d2fe4da6cbefb50619604d4ef6d3af77d0f3f8f
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-19 19:01:38 +0200

    Update evaluation_cpu.sh

--- colab/evaluation_cpu.sh
+++ colab/evaluation_cpu.sh
@@ -5,4 +5,4 @@ echo "Model to use: $3"
 echo "Learning rate: $4"
 
 echo "====================================== Start evaluation for batch $i ======================================"
-	  python3 $WORK_DIR_PREFIX/test.py /content/drive/My\ Drive/DBpedia/1/0/ /content/drive/My\ Drive/DBpedia/1/0/model/ $WORK_DIR_PREFIX/release/Base.so $2 $3 $5 | tee /content/drive/My\ Drive/DBpedia/1/0/res.txt
+	  python3 $WORK_DIR_PREFIX/test.py /content/drive/My\ Drive/DBpedia/1/0/ /content/drive/My\ Drive/DBpedia/1/0/model/ $WORK_DIR_PREFIX/release/Base.so $2 $3 | tee /content/drive/My\ Drive/DBpedia/1/0/res.txt
