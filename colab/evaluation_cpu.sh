fd8b8e96ead1ef2105f46799d61a60d42e0e4fbd
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-19 17:52:12 +0200

    Create evaluation_cpu.sh

---
+++ colab/evaluation_cpu.sh
@@ -0,0 +1,10 @@
+echo "====================================== Parameters ======================================"
+echo "Number of batches: $1"
+echo "Embedding dimensionality: $2"
+echo "Model to use: $3"
+echo "Learning rate: $4"
+echo "Target relation: $5"
+
+echo "====================================== Start evaluation for batch $i ======================================"
+	  echo "Evaluation with test"
+	  python3 $WORK_DIR_PREFIX/test.py /content/drive/My\ Drive/DBpedia/$n/$i/ /content/drive/My\ Drive/DBpedia/$n/$i/model/ $WORK_DIR_PREFIX/release/Base.so $2 $3 $5 | tee /content/drive/My\ Drive/DBpedia/$n/$i/res.txt
