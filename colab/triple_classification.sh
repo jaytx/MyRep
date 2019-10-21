925e685dc7c591d0d65070c1ddfeee95e379e87e
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-21 16:03:58 +0200

    Update triple_classification.sh

--- colab/triple_classification.sh
+++ colab/triple_classification.sh
@@ -7,5 +7,4 @@ echo "Model to use: $4"
 echo "====================================== Start Triple Classification Evaluation for batch $i ======================================"
 	  python3 $WORK_DIR_PREFIX/test.py $1 $2 $WORK_DIR_PREFIX/release/Base.so $3 $4 | tee $1/res.txt
 
-done
 
