51e8ae0b3017dea2ff50c1abf86dec5f9378c68f
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-21 16:16:21 +0200

    Update triple_classification.sh

--- colab/triple_classification.sh
+++ colab/triple_classification.sh
@@ -1,10 +1,8 @@
 echo "====================================== Parameters ======================================"
-echo "Dataset Path:" $1
-echo "Model Path:" $1
-echo "Embedding dimensionality: $3"
-echo "Model to use: $4"
+echo "Embedding dimensionality: $1"
+echo "Model to use: $2"
 
 echo "====================================== Start Triple Classification Evaluation for batch $i ======================================"
-	  python3 $WORK_DIR_PREFIX/test.py $1 $2 $WORK_DIR_PREFIX/release/Base.so $3 $4 | tee $1/res.txt
+	  python3 $WORK_DIR_PREFIX/test.py /content/drive/My\ Drive/DBpedia/$2 /content/drive/My\ Drive/DBpedia/$2/model $WORK_DIR_PREFIX/release/Base.so $1 $2 | tee $1/res.txt
 
 
