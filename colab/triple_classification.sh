4e6d1e34c16967d9d2b6d4a2d06d78d1365e2c4e
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-21 15:59:51 +0200

    Create triple_classification.sh

---
+++ colab/triple_classification.sh
@@ -0,0 +1,11 @@
+echo "====================================== Parameters ======================================"
+echo "Dataset Path:" $1
+echo "Model Path:" $1
+echo "Embedding dimensionality: $3"
+echo "Model to use: $4"
+
+echo "====================================== Start Triple Classification Evaluation for batch $i ======================================"
+	  python3 $WORK_DIR_PREFIX/test.py $1 $2 $WORK_DIR_PREFIX/release/Base.so $3 $4 | tee $1/res.txt
+
+done
+
