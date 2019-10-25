fb4c40fc31d6018d1b26b4aca686e963a40d14db
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-25 21:06:59 +0200

    Update Test.h

--- base/Test.h
+++ base/Test.h
@@ -350,7 +350,6 @@ void test_triple_classification(REAL *relThresh, REAL *score_pos, REAL *score_ne
     INT TP = 0, TN = 0, FP = 0, FN = 0;
 
     for (INT r = 0; r < relationTotal; r++) {
-        printf("Threshold is %f\n", relThresh[r]);
         if (validLef[r] == -1 || testLef[r] ==-1) continue;
         INT correct = 0, total = 0;
         for (INT i = testLef[r]; i <= testRig[r]; i++) {
