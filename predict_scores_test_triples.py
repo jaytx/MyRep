3a755d662dcc8e1d5ea9766dbb33f1b0052b8bea
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-28 18:08:32 +0100

    Update predict_scores_test_triples.py

--- predict_scores_test_triples.py
+++ predict_scores_test_triples.py
@@ -59,8 +59,9 @@ with open (os.path.join(dataset_path,'entity2id.txt')) as f_entity:
     f_entity.readline()
     for line in f_entity:
         entity=line.split("\t")
-        print(entity[0].split('"'))
-        entity_map.update({int(entity[1].split("\n")[0]) : entity[0].split('"')[1]})
+        if(len(entity[0].split('"'))>1):
+            entity_map.update({int(entity[1].split("\n")[0]) : entity[0].split('"')[1]})
+        else: entity_map.update({int(entity[1].split("\n")[0]) : entity[0]})
         
 rel_map={}
 with open((os.path.join(dataset_path,'relation2id.txt'))) as f_rel:
