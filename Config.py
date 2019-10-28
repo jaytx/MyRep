9a14267e2f34241ef19aec738c65ed56cb9ec780
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-28 15:01:51 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -510,6 +510,10 @@ class Config(object):
                 
                     res_pos = self.test_step(self.test_pos_h, self.test_pos_t, self.test_pos_r)
                     res_neg = self.test_step(self.test_neg_h, self.test_neg_t, self.test_neg_r)
+                    print("RES POS")
+                    print(res_pos)
+                    print("RES NEG")
+                    print(res_neg)
 
                     self.lib.test_triple_classification(self.relThresh_addr, res_pos.__array_interface__['data'][0], res_neg.__array_interface__['data'][0], self.acc_addr)
                 
