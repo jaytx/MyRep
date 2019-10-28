42a3f767c527c2d2b170e51fa1ad10549543fd20
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-28 11:04:35 +0100

    Update triple_classification.sh

--- colab/triple_classification.sh
+++ colab/triple_classification.sh
@@ -3,6 +3,6 @@ echo "Embedding dimensionality: $1"
 echo "Model to use: $2"
 
 echo "====================================== Start Triple Classification Evaluation for batch $i ======================================"
-	  python3 $WORK_DIR_PREFIX/test.py /content/drive/My\ Drive/DBpedia/1/0/ /content/drive/My\ Drive/DBpedia/1/0/model/ $WORK_DIR_PREFIX/release/Base.so $1 $2 | tee /content/drive/My\ Drive/DBpedia/1/0/model/res.txt
+	  python3 $WORK_DIR_PREFIX/predict_scores_test_triples.py /content/drive/My\ Drive/DBpedia/1/0/ /content/drive/My\ Drive/DBpedia/1/0/model/ $WORK_DIR_PREFIX/release/Base.so $1 $2 | tee /content/drive/My\ Drive/DBpedia/1/0/model/res.txt
 
 
