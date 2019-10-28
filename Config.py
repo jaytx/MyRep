df82cf9376066f555e1b4fbefd8ee2526b2ee9d7
jaytx <56222970+jaytx@users.noreply.github.com>
2019-10-28 16:51:38 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -510,11 +510,13 @@ class Config(object):
                     self.lib.getTestBatch(self.test_pos_h_addr, self.test_pos_t_addr, self.test_pos_r_addr, self.test_neg_h_addr, self.test_neg_t_addr, self.test_neg_r_addr)
                 
                     res_pos = self.test_step(self.test_pos_h, self.test_pos_t, self.test_pos_r)
+                    print("POS")
+                    print(self.test_pos_r)
                     test_tail_triples=self.test_pos_r
 
                     res_neg = self.test_step(self.test_neg_h, self.test_neg_t, self.test_neg_r)
-                    print("pre class")
-                
+                    print("NEG")
+                    print(self.test_neg_r)
                     self.lib.test_triple_classification(self.relThresh_addr, res_pos.__array_interface__['data'][0], res_neg.__array_interface__['data'][0], self.acc_addr)
                 test_time_elapsed = time.time() - test_time_start
                 print("\nElapsed test time (seconds): {}".format(test_time_elapsed))
