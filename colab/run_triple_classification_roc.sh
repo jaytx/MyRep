9b5dbd786fb286a488435138941632fc82479dca
jaytx <56222970+jaytx@users.noreply.github.com>
2019-11-02 10:26:57 +0100

    Update run_triple_classification_roc.sh

--- colab/run_triple_classification_roc.sh
+++ colab/run_triple_classification_roc.sh
@@ -73,4 +73,4 @@ do
 done
 
 echo "====================================== Start Triple Classification Evaluation ======================================"
-python3 $WORK_DIR_PREFIX/predict_scores_test_triples.py /content/drive/My\ Drive/DBpedia/1/0/ /content/drive/My\ Drive/DBpedia/1/0/model/ release/Base.so $2 $3 | tee /content/drive/My\ Drive/DBpedia/1/0/res.txt
+python3 $WORK_DIR_PREFIX/predict_scores_test_triples.py /content/drive/My\ Drive/DBpedia/$n/$m/ /content/drive/My\ Drive/DBpedia/$n/$m/model/ release/Base.so $2 $3 | tee /content/drive/My\ Drive/DBpedia/$n/$m/res.txt
